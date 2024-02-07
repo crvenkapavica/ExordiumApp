3b9d986687c96f74090977893fb5f13ba762567e
crvenkapavica <fmatjasec94@gmail.com>
2024-02-07 14:42:40 +0100

    PC lag

--- ExordiumApp/Assets/Scripts/Managers/OverlayManager.cs
+++ ExordiumApp/Assets/Scripts/Managers/OverlayManager.cs
@@ -26,7 +26,11 @@ public class OverlayManager : MonoBehaviour
 
         //for mBOX
         var rect = _outterOverlayPanel.GetComponent<RectTransform>();
-        rect.sizeDelta = new Vector2(rect.sizeDelta.x, Screen.height * 0.15f);
+        //rect.sizeDelta = new Vector2(rect.sizeDelta.x, rect.sizeDelta.y - Screen.height * 0.9f);
+        //rect.sizeDelta = new Vector2(Screen.width, Screen.height);
+        rect.anchorMin = new Vector2(0.075f, 0.425f);
+        rect.anchorMax = new Vector2(0.925f, 0.575f);
+        //rect.sizeDelta = new Vector2(1, Screen.height * 0.4f);
 
         _innerOverlayPanel.SetActive(true);
         foreach (var o in _overlays)
